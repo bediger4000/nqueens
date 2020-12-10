a8aa218debe7f86142040dbb7e0e7b40617e75c6
Bruce Ediger <bediger@apple.com>
2020-12-10 11:23:40 -0700

    Working solution

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+)
+
+const (
+	QUEEN  = -1
+	EMPTY  = 0
+	MARK   = 1
+	UNMARK = -1
 )
 
 func main() {
@@ -18,37 +26,52 @@ func main() {
 
 	for i := 0; i < *size; i++ {
 		for j := 0; j < *size; j++ {
-			setQueen(1, *size, &board, i, j)
+			board[i][j] = QUEEN
+			markSquares(*size, &board, i, j, 1)
+			checkBoard(1, *size, &board)
+			markSquares(*size, &board, i, j, -1)
+			board[i][j] = EMPTY
 		}
 	}
+	fmt.Printf("%d unique %d-queens boards\n", uniqueBoardCount, *size)
 }
 
-func setQueen(n, size int, board *[][]int) {
-	if n == size {
-		printBoard(board)
+var uniqueBoards = make(map[string]bool)
+var uniqueBoardCount int
+
+func stringify(board *[][]int) string {
+	buf := bytes.Buffer{}
+	for _, row := range *board {
+		for _, x := range row {
+			mark := byte('.')
+			if x == QUEEN {
+				mark = byte('Q')
+			}
+			buf.WriteByte(mark)
+		}
+	}
+	return buf.String()
+}
+
+func checkBoard(N, size int, board *[][]int) {
+	if N == size {
+		// All queens placed, base recursion case
+		boardAsString := stringify(board)
+		if !uniqueBoards[boardAsString] {
+			printBoard(board)
+			uniqueBoards[boardAsString] = true
+			uniqueBoardCount++
+		}
 		return
 	}
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
-			if (*board)[i][j] == 0 {
-				// place queen
-				(*board)[i][j] = -1
-				// increment all this queen's accessible squares
-				for x := 0; x < size; x++ {
-					if x != i {
-						(*board)[x][j]++
-					}
-				}
-				for x := 0; x < size; x++ {
-					if x != j {
-						(*board)[i][x]++
-					}
-				}
-				// call setQueen
-				setQueen(n+1, size, board)
-				// decrement all this queen's accessible squares
-				// remove queen
-				(*board)[i][j] = 0
+			if (*board)[i][j] == EMPTY {
+				(*board)[i][j] = QUEEN
+				markSquares(size, board, i, j, MARK)
+				checkBoard(N+1, size, board)
+				markSquares(size, board, i, j, UNMARK)
+				(*board)[i][j] = EMPTY
 			}
 		}
 	}
@@ -57,12 +80,98 @@ func setQueen(n, size int, board *[][]int) {
 func printBoard(board *[][]int) {
 	for _, row := range *board {
 		for _, x := range row {
-			marker := '_'
-			if x == -1 {
+			marker := '.'
+			if x == QUEEN {
 				marker = 'Q'
 			}
 			fmt.Printf("%c", marker)
 		}
 		fmt.Println()
+	}
+	fmt.Println()
+	fmt.Println()
+}
+
+func printRawBoard(board *[][]int) {
+	for _, row := range *board {
+		for _, x := range row {
+			fmt.Printf("%2d", x)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func markSquares(size int, board *[][]int, p, q, mark int) {
+	// row with <p,q> in it
+	for i := -size; i < size; i++ {
+		if i == 0 {
+			continue
+		}
+		n := q + i
+		if n < 0 {
+			continue
+		}
+		if n >= size {
+			continue
+		}
+		(*board)[p][n] += mark
+	}
+	// col with <p,q> in it
+	for i := -size; i < size; i++ {
+		if i == 0 {
+			continue
+		}
+		m := p + i
+		if m < 0 {
+			continue
+		}
+		if m >= size {
+			continue
+		}
+		(*board)[m][q] += mark
+	}
+
+	// diagonal, lower left to upper right
+	for i := -size; i < size; i++ {
+		if i == 0 {
+			continue
+		}
+		m := p + i
+		if m < 0 {
+			continue
+		}
+		if m >= size {
+			continue
+		}
+		n := q + i
+		if n < 0 {
+			continue
+		}
+		if n >= size {
+			continue
+		}
+		(*board)[m][n] += mark
+	}
+	// diagonal, upper left to lower right
+	for i := -size; i < size; i++ {
+		if i == 0 {
+			continue
+		}
+		m := p - i
+		if m < 0 {
+			continue
+		}
+		if m >= size {
+			continue
+		}
+		n := q + i
+		if n < 0 {
+			continue
+		}
+		if n >= size {
+			continue
+		}
+		(*board)[m][n] += mark
 	}
 }
