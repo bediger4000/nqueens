2c6648778f7ed3d6106d2e5f703b53a368571f74
Bruce Ediger <bediger8@gmail.com>
2021-01-15 14:06:01 -0700

    faster empty/unattacked square checks

--- recursive.go
+++ recursive.go
@@ -113,45 +113,18 @@ func markSquares(size int, board *[12][12]int, p, q, mark int) {
 	}
 
 	// diagonal, lower left to upper right
-	for i := -size; i < size; i++ {
-		if i == 0 {
-			continue
-		}
-		m := p + i
-		if m < 0 {
-			continue
-		}
-		if m >= size {
-			continue
-		}
-		n := q + i
-		if n < 0 {
-			continue
-		}
-		if n >= size {
-			continue
-		}
-		(*board)[m][n] += mark
+	for i := 1; p-i >= 0 && q-i >= 0; i++ {
+		(*board)[p-i][q-i] += mark
 	}
+	for i := 1; p+i < size && q+i < size; i++ {
+		(*board)[p+i][q+i] += mark
+	}
+
 	// diagonal, upper left to lower right
-	for i := -size; i < size; i++ {
-		if i == 0 {
-			continue
-		}
-		m := p - i
-		if m < 0 {
-			continue
-		}
-		if m >= size {
-			continue
-		}
-		n := q + i
-		if n < 0 {
-			continue
-		}
-		if n >= size {
-			continue
-		}
-		(*board)[m][n] += mark
+	for i := 1; p+i < size && q-i >= 0; i++ {
+		(*board)[p+i][q-i] += mark
+	}
+	for i := 1; p-i >= 0 && q+i < size; i++ {
+		(*board)[p-i][q+i] += mark
 	}
 }
